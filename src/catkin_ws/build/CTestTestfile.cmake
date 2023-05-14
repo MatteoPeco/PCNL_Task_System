a9eecda37f3b7e39501983712cf77dc9789a2ea8
Matteo Pecorella <94977542+MatteoPeco@users.noreply.github.com>
2023-05-14 11:53:42 +0200

    Add files via upload

---
+++ src/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,20 @@
+# CMake generated Testfile for 
+# Source directory: /home/poskuka-nearlab/catkin_ws/src
+# Build directory: /home/poskuka-nearlab/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(kuka-lwr/kuka_lwr)
+subdirs(kuka-lwr/lwr_description)
+subdirs(kuka-lwr/single_lwr_example/single_lwr_example)
+subdirs(kuka-lwr/single_lwr_example/single_lwr_launch)
+subdirs(kuka-lwr/single_lwr_example/single_lwr_robot)
+subdirs(ros_packages/unity_robotics_demo_msgs)
+subdirs(ROS-TCP-Endpoint)
+subdirs(kuka-lwr/lwr_robot_tool)
+subdirs(ros_packages/unity_robotics_demo)
+subdirs(kuka-lwr/lwr_hw)
+subdirs(kuka-lwr/lwr_controllers)
+subdirs(cartesian_position_node)
+subdirs(kuka-lwr/single_lwr_example/single_lwr_moveit)
