da1c4f96ee648edc81e77889eeb8c02d7486f875
Matteo Pecorella <94977542+MatteoPeco@users.noreply.github.com>
2023-05-14 12:03:29 +0200

    Add files via upload

---
+++ src/catkin_ws/build/ros_packages/unity_robotics_demo_msgs/CMakeFiles/unity_robotics_demo_msgs_generate_messages_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unity_robotics_demo_msgs_generate_messages_cpp"
+  "/home/poskuka-nearlab/catkin_ws/devel/include/unity_robotics_demo_msgs/UnityColor.h"
+  "/home/poskuka-nearlab/catkin_ws/devel/include/unity_robotics_demo_msgs/RotJoints.h"
+  "/home/poskuka-nearlab/catkin_ws/devel/include/unity_robotics_demo_msgs/PosRot.h"
+  "/home/poskuka-nearlab/catkin_ws/devel/include/unity_robotics_demo_msgs/ObjectPoseService.h"
+  "/home/poskuka-nearlab/catkin_ws/devel/include/unity_robotics_demo_msgs/PositionService.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/unity_robotics_demo_msgs_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/catkin_ws/build/ros_packages/unity_robotics_demo_msgs/CMakeFiles/unity_robotics_demo_msgs_generate_messages_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unity_robotics_demo_msgs_generate_messages_lisp"
+  "/home/poskuka-nearlab/catkin_ws/devel/share/common-lisp/ros/unity_robotics_demo_msgs/msg/UnityColor.lisp"
+  "/home/poskuka-nearlab/catkin_ws/devel/share/common-lisp/ros/unity_robotics_demo_msgs/msg/RotJoints.lisp"
+  "/home/poskuka-nearlab/catkin_ws/devel/share/common-lisp/ros/unity_robotics_demo_msgs/msg/PosRot.lisp"
+  "/home/poskuka-nearlab/catkin_ws/devel/share/common-lisp/ros/unity_robotics_demo_msgs/srv/ObjectPoseService.lisp"
+  "/home/poskuka-nearlab/catkin_ws/devel/share/common-lisp/ros/unity_robotics_demo_msgs/srv/PositionService.lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/unity_robotics_demo_msgs_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
