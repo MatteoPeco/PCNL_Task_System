d072834701422f1752716d2d70f4f9465ea77eeb
Matteo Pecorella <94977542+MatteoPeco@users.noreply.github.com>
2023-05-14 11:59:55 +0200

    Add files via upload

---
+++ src/catkin_ws/build/ros_packages/unity_robotics_demo_msgs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/poskuka-nearlab/catkin_ws/src/ros_packages/unity_robotics_demo_msgs
+# Build directory: /home/poskuka-nearlab/catkin_ws/build/ros_packages/unity_robotics_demo_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ src/catkin_ws/build/ros_packages/unity_robotics_demo_msgs/cmake/unity_robotics_demo_msgs-genmsg-context.py
@@ -0,0 +1,11 @@
+# generated from genmsg/cmake/pkg-genmsg.context.in
+
+messages_str = "/home/poskuka-nearlab/catkin_ws/src/ros_packages/unity_robotics_demo_msgs/msg/PosRot.msg;/home/poskuka-nearlab/catkin_ws/src/ros_packages/unity_robotics_demo_msgs/msg/UnityColor.msg;/home/poskuka-nearlab/catkin_ws/src/ros_packages/unity_robotics_demo_msgs/msg/RotJoints.msg"
+services_str = "/home/poskuka-nearlab/catkin_ws/src/ros_packages/unity_robotics_demo_msgs/srv/ObjectPoseService.srv;/home/poskuka-nearlab/catkin_ws/src/ros_packages/unity_robotics_demo_msgs/srv/PositionService.srv"
+pkg_name = "unity_robotics_demo_msgs"
+dependencies_str = "geometry_msgs;std_msgs"
+langs = "gencpp;geneus;genlisp;gennodejs;genpy"
+dep_include_paths_str = "unity_robotics_demo_msgs;/home/poskuka-nearlab/catkin_ws/src/ros_packages/unity_robotics_demo_msgs/msg;geometry_msgs;/opt/ros/kinetic/share/geometry_msgs/cmake/../msg;std_msgs;/opt/ros/kinetic/share/std_msgs/cmake/../msg"
+PYTHON_EXECUTABLE = "/usr/bin/python2"
+package_has_static_sources = '' == 'TRUE'
+genmsg_check_deps_script = "/opt/ros/kinetic/share/genmsg/cmake/../../../lib/genmsg/genmsg_check_deps.py"
